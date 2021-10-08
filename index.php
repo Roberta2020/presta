21dad13ad2d33a0a4c51304aeb8f0ea229954674
Roberta2020 <74533119+Roberta2020@users.noreply.github.com>
2021-10-08 12:42:31 +0300

    HTML tags

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 // 1. 
 
 $table = array(
@@ -131,7 +139,14 @@ print_r ($group);
  
 // subsetSums($arr, 0, $n - 1);
 
-?>
+?>    
+
+
+</body>
+</html>
+
+
+
 
 
 
